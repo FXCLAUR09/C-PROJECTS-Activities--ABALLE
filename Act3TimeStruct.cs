24d21fac480834a16fbdb920cacf7dab6b55d6a8
John Laurence E. Aballe <jlaurenceaballe1823@gmail.com>
2025-06-12 22:30:37 +0800

    Add files via upload

---
+++ Act3TimeStruct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+struct Time
+{
+    private readonly int minutes;
+
+    public Time(int hh, int mm)
+    {
+        minutes = 60 * hh + mm;
+    }
+
+    public override string ToString()
+    {
+        return $"{minutes} minutes past midnight";
+    }
+   
+}
+
+class Program
+{
+    static void Main()
+    {
+        Time t1 = new Time(10, 5);  // 10:05
+        Time t2 = new Time(0, 45);  // 00:45
+
+        Console.WriteLine(t1);  // Output: 605
+        Console.WriteLine(t2);  // Output: 45
+    }
+}
